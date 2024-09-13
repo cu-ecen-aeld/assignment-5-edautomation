e773ceff335ed6b67d35fe89965a55242d77670b
Emile Decosterd <edautomation20@gmail.com>
2024-09-13 14:32:56 +0200

    Update git hash of assignment repo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '0f0d6de9cb2889f09a2a2d5666d3faa6f3ad32e7'
+AESD_ASSIGNMENTS_VERSION = '9de02da02eec3cf83b74746cdf363df65a0860c1'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
