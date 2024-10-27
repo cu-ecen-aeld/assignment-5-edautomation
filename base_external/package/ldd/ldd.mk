61e4b903cb4f8a77aa652b1fc2edfbec424fd007
Emile Decosterd <edautomation20@gmail.com>
2024-10-27 22:34:23 +0100

    WIP

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,19 +9,14 @@ LDD_VERSION = 'fad8f769d9a10c67f25b44d431e336afe5506773'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = 'git@github.com:cu-ecen-aeld/assignments-7-edautomation.git'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-edautomation.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 # LDD_MODULE_SUBDIRS = scull misc_modules
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc_modules
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull
-endef
-
 define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/misc_modules/module_load $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/misc_modules/module_unload $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/
 endef
