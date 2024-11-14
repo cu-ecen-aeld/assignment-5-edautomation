c0aa7a8accda939a957fe593aa764c2193df1b11
Emile Decosterd <edautomation20@gmail.com>
2024-11-14 12:39:58 +0100

    Update ref to ass3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'e4519facf03e1ff1d8d59107fdfb4775dee89aeb'
+AESD_ASSIGNMENTS_VERSION = '44300008f77bdcfce1d7c761c5147334b0449bb6'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
