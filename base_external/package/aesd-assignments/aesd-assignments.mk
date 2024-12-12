4212ad90414a363cf2d9c5858ebe5523ba4f1b1a
Emile Decosterd <edautomation20@gmail.com>
2024-12-12 10:10:46 +0100

    Update repo ref

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'cc00d566c82789a639b113bbc33d9dc70e1ce382'
+AESD_ASSIGNMENTS_VERSION = '45fc7bf495b0436ee1aa2986326f7369d252d20a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
